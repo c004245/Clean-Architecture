f32d2463a1e965323fcec9f32c1feae4bf2a8a84
hyunwook <c004245@naver.com>
2019-09-15 16:53:27 +0900

    Result Success, fail처리 MutableLiveData 셋

--- app/src/main/java/hyunwook/co/kr/clean_architecture/viewmodel/BeersViewModel.kt
+++ app/src/main/java/hyunwook/co/kr/clean_architecture/viewmodel/BeersViewModel.kt
@@ -6,17 +6,31 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import hyunwook.co.kr.clean_architecture.commons.Result
 import hyunwook.co.kr.clean_architecture.commons.ResultType
+import hyunwook.co.kr.clean_architecture.domain.GetBeersUseCase
 import hyunwook.co.kr.clean_architecture.domain.model.BeersEntity
+import hyunwook.co.kr.clean_architecture.viewmodel.mapper.ViewModelMapper
 import hyunwook.co.kr.clean_architecture.viewmodel.model.BeerUI
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class BeersViewModel : ViewModel() {
+class BeersViewModel(private val getBeersUseCase: GetBeersUseCase) : ViewModel() {
 
-    private val beersLiveData = MutableLiveData<List<BeerUI>>()
-    private val isLoadingLiveData = MutableLiveData<Boolean>()
+    private val beersLiveData = MutableLiveData<List<BeerUI>>() //Beer Info LiveData
+    private val areEmptyBeersLiveData = MutableLiveData<Boolean>() //Empty Beer LiveData
+    private val isLoadingLiveData = MutableLiveData<Boolean>() //Loading Progress LiveData
+    private val isErrorLiveData = MutableLiveData<Boolean>() //Error LiveData
+
+    val beers: MutableLiveData<List<BeerUI>>
+        get() = beersLiveData
+
+    val areEmptyBeers: MutableLiveData<Boolean>
+        get() = areEmptyBeersLiveData
 
     val isLoading: MutableLiveData<Boolean>
         get() = isLoadingLiveData
+
+    val isError: MutableLiveData<Boolean>
+        get() = isErrorLiveData
 
     init {
         handleBeersLoad()
@@ -26,7 +40,7 @@ class BeersViewModel : ViewModel() {
         isLoadingLiveData(true)
         viewModelScope.launch {
             Log.d("TEST", "viewModelScope launch...")
-            updateAppropriateLiveData()
+            updateAppropriateLiveData(getBeersUseCase.execute())
         }
     }
 
@@ -43,8 +57,25 @@ class BeersViewModel : ViewModel() {
     }
 
     private fun onResultSuccess(beersEntity: BeersEntity?) {
-        val beers = ViewModelMa
+        val beers = ViewModelMapper.EntityToUI.map(beersEntity?.beers)
+
+        if (beers.isEmpty()) {
+            areEmptyBeersLiveData.postValue(true)
+        } else {
+            beersLiveData.postValue(beers)
+        }
+        isLoadingLiveData(false)
     }
+
+    private fun onResultError() {
+        viewModelScope.launch {
+            delay(300)
+            isLoadingLiveData(false)
+        }.invokeOnCompletion {
+            isErrorLiveData.postValue(true)
+        }
+    }
+
 
     private fun isLoadingLiveData(isLoading: Boolean) {
         this.isLoadingLiveData.postValue(isLoading)
